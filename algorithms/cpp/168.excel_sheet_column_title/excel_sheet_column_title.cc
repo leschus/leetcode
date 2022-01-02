1d1c1d2d0f020c4f825d9791fc8700d8b782f918
leschus <leschus@foxmail.com>
2022-01-02 22:35:15 +0800

    168

---
+++ algorithms/cpp/168.excel_sheet_column_title/excel_sheet_column_title.cc
@@ -0,0 +1,43 @@
+// Source: https://leetcode-cn.com/problems/excel-sheet-column-title
+// Author: Shihao Liu
+// Date: 2022-01-02
+
+#include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution_1 {
+ public:
+  /**
+  * @brief 26进制
+  *
+  * @时间复杂度 O(logn), 底数为26
+  */
+  string converToTitle(int columnNumber) {
+    char cs[10];
+    int i = 0;
+    while (columnNumber) {
+      --columnNumber;
+      cs[i] = columnNumber % 26 + 'A';
+      columnNumber /= 26;
+      ++i;
+    }
+    cs[i] = '\0';
+    string s(cs);
+    reverse(s.begin(), s.end());
+    return s;
+  }
+
+};
+
+
+int main() {
+  int n1 = 1, n2 = 28, n3 = 701, n4 = 2147483647;
+  Solution_1 sol;
+  cout << sol.converToTitle(n1) << endl;
+  cout << sol.converToTitle(n2) << endl;
+  cout << sol.converToTitle(n3) << endl;
+  cout << sol.converToTitle(n4) << endl;
+
+}
